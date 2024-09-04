84ce8a0ae96a5eca79d3e730814e09ac6338178a
Samuel <samuelqlopes99@gmail.com>
2024-09-03 21:48:42 -0300

    implement simple CPU matmul on tensors

--- tensor.c
+++ tensor.c
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include "./tensor.h"
 
+// allocate a tensor in memory
 Tensor create_tensor(int ndim, int *dims) {
     Tensor tensor;
     tensor.ndim = ndim;
@@ -33,13 +34,13 @@ void init_ones(Tensor *t) {
 }
 
 void init_random(Tensor *t) {
-    srand(time(NULL));
+    srand(time(0));
     for (int i = 0; i < t->size; i++) {
-        t->data[i] = (float)(rand() / RAND_MAX); // random value between 0 and 1
+        t->data[i] = (float)rand() / (float)RAND_MAX; // random value between 0 and 1
     }
 }
 
-// Arithmetic Ops: (add, sub, mul, div)
+// Arithmetic Ops: (add, mul)
 void add_tensor(Tensor *t1, Tensor *t2, Tensor *out, int size) {
     assert(t1->size == size && t2->size == size);
     for (int i = 0; i < size; i++) {
@@ -54,41 +55,44 @@ void mul_tensor(Tensor *t1, Tensor *t2, Tensor *out, int size) {
     }
 }
 
-/*
-// CPU matmul
-void matmul_tensor(Tensor *t1, Tensor *t2, Tensor *out, int size, int dim) {
-    float sum = 0.0f;
-    if (dim == 3) {
-        assert(t1->dims[2] == t2->dims[0]);
-    } else {
-        assert(t1->dims[1] == t2->dims[0]);
-        for (int i = 0; i < t1->dims[0]; i++) {
-            for (int j = 0; j < t2->dims[1]; j++) {
-                //sum += t1->data[i] * t2->data[j];
-                for (int k = 0; k < t2->dims[0]; j++) {
-                    sum += t1->data[i*j+k] * 
-                }
+// CPU matmul (2d tensor only for now!)
+void matmul_tensor(Tensor *t1, Tensor *t2, Tensor *out) {
+    assert(t1->dims[1] == t2->dims[0]);
+    for (int i = 0; i < t1->dims[0]; i++) {
+        for (int j = 0; j < t2->dims[1]; j++) {
+            for (int k = 0; k < t1->dims[1]; k++) {
+                out->data[i * (t2->dims[1]) + j] += t1->data[i*(t1->dims[1])+k] * t2->data[k*(t2->dims[1])+j]; 
             }
-            out->data[i] = sum;
-        } 
-    }   
-}*/
+        }
+    } 
+} 
+
+void print_tensor(Tensor *t, int size, int *dims) {
+    for (int i = 0; i < size; i++) {
+        printf("%f ", t->data[i]);
+        if ((i+1) % dims[1] == 0) printf("\n");
+    }
+}
 
 int main() {
     // example test of creating a initializing a Tensor
-    int dims[3] = {3,3,2};
-    Tensor tensor1 = create_tensor(3, dims);
+    int dims[] = {3,3};
+    Tensor tensor1 = create_tensor(2, dims);
     init_ones(&tensor1);
-    Tensor tensor2 = create_tensor(3, dims);
-    init_ones(&tensor2);
-    Tensor out = create_tensor(3, dims);
-    add_tensor(&tensor1, &tensor2, &out, out.size);
-    
-    for (int i = 0; i < out.size; i++) {
-        printf("%f ", out.data[i]);
-        if ((i+1) % dims[2] == 0) printf("\n");
-    }
+    Tensor tensor2 = create_tensor(2, dims);
+    init_random(&tensor2);
+    Tensor out = create_tensor(2, dims);
+    init_zeros(&out);
+    //add_tensor(&tensor1, &tensor2, &out, out.size);
 
+    matmul_tensor(&tensor1, &tensor2, &out);
+
+    printf("tensor 1:\n");
+    print_tensor(&tensor1, tensor1.size, dims);
+    printf("tensor 2:\n");
+    print_tensor(&tensor2, tensor2.size, dims);
+    printf("tensor out:\n");
+    print_tensor(&out, out.size, dims);
 
 
     free(tensor1.data);
