0cafd6fe4c191d6d68618c5077115d053a31ebf5
Samuel <samuelqlopes99@gmail.com>
2024-09-16 23:33:09 -0300

    start layernorm implementation

--- transformer.c
+++ transformer.c
@@ -12,6 +12,16 @@
 #define BLOCK_SZ 8 // T
 #define EMBD_SZ 4  // C
 
+/*
+typedef struct {
+    int block_size = 256, // 1024 (actual gpt2 config)
+    int vocab_size = 65,  // 50257  
+    int n_layer = 6,      // 12
+    int n_head = 6,       // 12
+    int n_embd = 384,     // 768
+} GPTConfig;
+*/
+
 char *read_file(char *filename) {
     FILE *fp = fopen(filename, "r");
     if (fp == NULL) return NULL;
@@ -50,24 +60,37 @@ void encoder(int B, int T, int C, float *wte, float *wpe, int *in, float *out) {
     }
 }
 
-void matmul_2d_cpu(float **m, float **n, float **out, int row, int col) {
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            out[i][j] = 0;
-            for (int k = 0; k < col; k++) {
-                out[i][j] += m[i][k] * n[k][j];
-            }
+// Layer normalization over the embedding dimmension for each token in the sequence
+void layernorm(int B, int T, int C, float *in, float *out, float beta, float gamma) {
+    for (int b = 0; b < B; b++) {
+        for (int t = 0; t < T; t++) {
+            float mean = 0.0f;
+            for (int c = 0; c < C; c++) {
+                mean += in[b * (T * C) + t * C + c];
+            }        
         }
     }
 }
 
-void matmul_cpu(float *m, float *n, float *out, int row, int col) {
-    for (int i = 0; i < row, i++) {
-        for (int j = 0; j < col; j++) {
-            m[i*row+j] * n[j*row+col] // m[i][j] * n[j][i]
+// simple cpu matmul calculation to compare with CUDA version
+void matmul_cpu(float *m, float *n, float *out, int row_m, int col_m, int col_n) {
+    float value = 0.0f;
+    for (int i = 0; i < row_m; i++) {
+        for (int j = 0; j < col_n; j++) {
+            //out[i*col_n+j] = 0.0f;
+            for (int k = 0; k < col_m; k++) {
+                //out[i*col_n+j] += m[i*col_m+k] * n[k*col_n+j];
+                value += m[i*col_m+k] * n[k*col_n+j];
+            }
+            out[i*col_n+j] = value;
         }
     }
 }
+
+// separate matmul into 2 functions so we can precisely calculate the time only in the matmul computation.
+//void call_matmul_cpu(float *m, float *n) {
+    // TODO: 
+//}
 
 float *init_matrix(int row, int col) {
     float *out = (float*)malloc(row * col * sizeof(float));
@@ -77,6 +100,7 @@ float *init_matrix(int row, int col) {
     return out;
 }
 
+/*
 void self_attention(int B, int T, int C, float *in, float *out) {
     float *wQ = init_matrix(C, C);
     float *wK = init_matrix(C, C);
@@ -89,6 +113,7 @@ void self_attention(int B, int T, int C, float *in, float *out) {
     }
         float *query = 
 }
+*/
 
 // function to split dataset tokens into train/test (90,10)%
 void split_data(int *tokens, int sz, int *train, int *test, int train_sz, int test_sz) {
@@ -134,7 +159,7 @@ int main() {
     
     // get vocab from input string and sort. For character tokenization we don't actually need a vocab. But eventually we'll implement a more efficient tokenization (BPE)
     char *vocab = get_vocab(input);
-    int vocab_sz = strlen(vocab);
+    int vocab_sz = strlen(vocab); // (65)
     qsort(vocab, vocab_sz, sizeof(char), compare);
     printf("vocab:\n%s\n%d\n", vocab, vocab_sz); // 1st char of vocab is '\n'
 
@@ -144,21 +169,21 @@ int main() {
     int input_sz = strlen(str); // n
     printf("n: %d\n", input_sz);
     int *tokens = (int*)malloc(input_sz * sizeof(int));
-    encode_tokens_v2(str, tokens, vocab, input_sz);
+    encode_tokens_v2(str, tokens, vocab, input_sz); // (input_sz)
     printf("tokens length: %d\n", input_sz);
 
     // train/test split
     int split = (int)(0.9*input_sz); // (split=90%) and (n-split=10%)
-    int *train_data = (int*)malloc(split * sizeof(int));
+    int *train_data = (int*)malloc(split * sizeof(int)); 
     int *test_data = (int*)malloc((input_sz-split) * sizeof(int));
     int train_sz = split, test_sz = input_sz-split;
     split_data(tokens, input_sz, train_data, test_data, train_sz, test_sz);
     //print_tokens(train_data, train_sz); print_tokens(test_data, test_sz);
     
     // generate a random batch of data (inputs: x, target: y)
-    int *x = (int*)malloc(BATCH_SZ * BLOCK_SZ * sizeof(int));
+    int *x = (int*)malloc(BATCH_SZ * BLOCK_SZ * sizeof(int)); // (B, T)
     int *y = (int*)malloc(BATCH_SZ * BLOCK_SZ * sizeof(int));
-    get_batch("train", train_data, test_data, train_sz, x, y, BATCH_SZ, BLOCK_SZ);
+    get_batch("train", train_data, test_data, train_sz, x, y, BATCH_SZ, BLOCK_SZ); // remember to do this multiple times throughout the dataset. Here's doing a single time, it works, but it has to do for the whole dataset
     /* (DEBUG)
     for (int i = 0; i < BATCH_SZ; i++) {
         printf("Batch %d:\n", i + 1);
@@ -175,19 +200,19 @@ int main() {
     }
     */
 
-    // test for token_embedding
-    float *wte = init_tok_emb_matrix(vocab_sz, C); // (65, 4). matrix is working fine
-    float *wpe = init_pos_emb_matrix(T, C);      // ( 8, 4)
+    // token + positional embeddings
+    float *wte = init_tok_emb_matrix(vocab_sz, C); // (65, 4). (vocab_sz, C)
+    float *wpe = init_pos_emb_matrix(T, C);      // (8, 4).    (T, C)
 
-    print_lookup_table("token", wte, vocab_sz, C);
-    print_lookup_table("pos", wpe, T, C);
-     
-    float *output_emb = (float*)malloc(B * T * C * sizeof(float));
+    //print_lookup_table("token", wte, vocab_sz, C);
+    //print_lookup_table("pos", wpe, T, C);
+
+    float *output_emb = (float*)malloc(B * T * C * sizeof(float)); // (B, T, C): batches, blocksize, n_embd
     int *tokens_id = x; // need to do to 'y' as well?
-    encoder(B, T, C, wte, wpe, tokens_id, output_emb);
+    encoder(B, T, C, wte, wpe, tokens_id, output_emb); // (B, T, C)
     
     // Print the result for the first token in the first batch (for demonstration purposes) 
-    for (int j = 0; j < T; j++) {
+    /*for (int j = 0; j < T; j++) {
         printf("\nEmbedding for the each T token in the first batch:\n");
         for (int i = 0; i < C; i++) {
             printf("%f ", output_emb[j * C + i]);
@@ -195,7 +220,7 @@ int main() {
         printf("\n");
     }
     printf("\n");
-    
+    */
     
     // Free the allocated memory
     free(wte);
